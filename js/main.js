d913c00b706fcdefebe709f90b8e12caab9c60e7
felixdevweb <trankhanh0525@gmail.com>
2023-11-25 13:32:06 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function bam() {
+    alert("Đăng kí thành công.")
+}
+
+function dev()
+{
+    alert("Tính năng đang được phát triển")
+}
+
+function feedback()
+{
+    alert("Phản hồi của bạn đã được ghi nhận")
+}
